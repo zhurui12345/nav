bf083445e1d16f27ea76e05fc0006f644d39ad30
zhurui12345 <70248339+zhurui12345@users.noreply.github.com>
2025-08-22 16:40:56 +0000

    Deploying to gh-pages from @ zhurui12345/nav@c5d0b5221495f411b15af81d26b72a2b65c1f767 🚀

---
+++ chunk-RALSTKAD.js
@@ -0,0 +1,1 @@
+import{c as a}from"./chunk-IIHL3SUF.js";import{qb as r,zb as e}from"./chunk-A5Z46WWS.js";var n=class t{constructor(i){this.sanitized=i}transform(i){return this.sanitized.bypassSecurityTrustHtml(i)}static \u0275fac=function(m){return new(m||t)(r(a,16))};static \u0275pipe=e({name:"safeHtml",type:t,pure:!0})};export{n as a};
